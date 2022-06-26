77930d8b629cdb94dc4d826e249333d1e7fd1e34
Dimags500 <73039506+Dimags500@users.noreply.github.com>
2022-06-26 15:19:13 +0300

    solution

--- solid/single_responsibility/problem.js
+++ solid/single_responsibility/problem.js
@@ -1,3 +1,5 @@
+const { PresistenceManager } = require("./solution");
+
 //class should have one reason to change / one responsibility
 // reason of change should be related to te this responsibility
 
@@ -22,25 +24,19 @@ class Journal {
     return Object.values(this.entries).join("\n");
   }
 
-  saveToFile(filename) {
-    // fs save implimentation
-  }
-  saveByUrl(url) {
-    // save by url save implimentation
-  }
-
-  loadFormFile(filename) {
-    //  load fs implimentation
-  }
-  loadFormUrl(url) {
-    // load by url  implimentation
+  getEntries() {
+    return this.entries;
   }
 }
 
 Journal.count = 0;
 
-let j = new Journal();
-j.addEntry("Hello j1");
-j.addEntry("J2 here");
+let journal = new Journal();
+journal.addEntry("Hello j1");
+journal.addEntry("J2 here");
+console.log(journal.toString());
 
-console.log(j.toString());
+const entries = journal.getEntries();
+const manager = new PresistenceManager();
+
+manager.saveToFile(entries, "path");
